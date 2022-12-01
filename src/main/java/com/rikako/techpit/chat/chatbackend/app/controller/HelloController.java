b486ef92a3ea29d507664ce11234870ab399cb26
rikako <jc180215b@gmail.com>
2022-12-01 15:38:16 +0900

    modify:HelloApiTest

--- src/main/java/com/rikako/techpit/chat/chatbackend/app/controller/HelloController.java
+++ src/main/java/com/rikako/techpit/chat/chatbackend/app/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.rikako.techpit.chat.chatbackend.app.controller;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rikako.techpit.chat.chatbackend.domain.hello.model.Hello;
 
 @RestController
 public class HelloController {
   @GetMapping("/hello")
-  public String hello(){
-    return "Hello,world!";
+  public Hello hello(@RequestParam("name") Optional<String> name){
+    String resName = name.orElse("world!");
+    return new Hello("Hello,"+ resName);
   }
 }
