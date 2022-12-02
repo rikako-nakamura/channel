5d7f15abf8270770625245a43fd8f679c34812cd
rikako <jc180215b@gmail.com>
2022-12-02 09:56:29 +0900

    modify:model_チャンネル作成機能

---
+++ src/main/java/com/rikako/techpit/chat/chatbackend/domain/hello/model/channels/model/Channel.java
@@ -0,0 +1,13 @@
+package com.rikako.techpit.chat.chatbackend.domain.hello.model.channels.model;
+
+import lombok.Data;
+
+@Data
+public class Channel {
+
+  //チャンネルID
+  private int id;
+  
+  //チャンネル名
+  private String name;
+}
